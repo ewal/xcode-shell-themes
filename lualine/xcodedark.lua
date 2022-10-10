2c697e485b5658be9efdac2a1cdd25b67fa11e4d
Emil Löfquist <lofquist@gmail.com>
2022-10-10 02:43:52 +0200

    Update and move files

---
+++ lualine/xcodedark.lua
@@ -0,0 +1,43 @@
+-- https://gist.github.com/shadmansaleh/000871c9a608a012721c6acc6d7a19b9
+-- stylua: ignore
+
+local colors = {
+  bg = "#292a30",
+  fg = "#dfdfe0",
+  black = "#414453",
+  red = "#ff8170",
+  green = "#78c2b3",
+  yellow = "#d9c97c",
+  blue = "#4eb0cc",
+  magenta = "#ff7ab2",
+  cyan = "#b281eb",
+  white = "#dfdfe0",
+}
+
+return {
+  inactive = {
+    a = { fg = colors.fg, bg = colors.black, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.black },
+    c = { fg = colors.fg, bg = colors.black },
+  },
+  normal = {
+    a = { fg = colors.white, bg = colors.green, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  visual = {
+    a = { fg = colors.white, bg = colors.blue, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  replace = {
+    a = { fg = colors.white, bg = colors.cyan, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  insert = {
+    a = { fg = colors.white, bg = colors.magenta, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+}
---
+++ lualine/xcodedarkhc.lua
@@ -0,0 +1,43 @@
+-- https://gist.github.com/shadmansaleh/000871c9a608a012721c6acc6d7a19b9
+-- stylua: ignore
+
+local colors = {
+  bg = "#1f1f24",
+  fg = "#ffffff",
+  black = "#43454b",
+  red = "#ff8a7a",
+  green = "#83c9bc",
+  yellow = "#d9c668",
+  blue = "#4ec4e6",
+  magenta = "#ff85b8",
+  cyan = "#cda1ff",
+  white = "#ffffff",
+}
+
+return {
+  inactive = {
+    a = { fg = colors.fg, bg = colors.black, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.black },
+    c = { fg = colors.fg, bg = colors.black },
+  },
+  normal = {
+    a = { fg = colors.white, bg = colors.green, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  visual = {
+    a = { fg = colors.white, bg = colors.blue, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  replace = {
+    a = { fg = colors.white, bg = colors.cyan, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  insert = {
+    a = { fg = colors.white, bg = colors.magenta, gui = "bold" },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white, bg = colors.black },
+  },
+}
